fa54c51279c2f76535e10c6a4a32ef98d2dafa2a
Oleg Zinoviev <ozinoviev@solit-clouds.ru>
2017-12-29 16:47:40 +0300

    drill 1.12.0

--- src/main/java/org/apache/drill/exec/store/rest/query/CompareProcessor.java
+++ src/main/java/org/apache/drill/exec/store/rest/query/CompareProcessor.java
@@ -91,7 +91,7 @@ class CompareProcessor extends AbstractExprVisitor<Boolean, LogicalExpression, R
             return false;
         }
 
-        boolean success = scan.getStoragePlugin().getRequestParameters().equalsIgnoreCase(path.getAsUnescapedPath());
+        boolean success = scan.getStoragePlugin().getRequestParameters().equalsIgnoreCase(path.getRootSegmentPath());
         return success && valueArg.accept(this, valueArg);
     }
 
